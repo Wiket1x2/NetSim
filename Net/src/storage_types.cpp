dfe024a8bde38a37853ba7da50b250aa84203f97
krzkaszinpg <49557289+krzkaszinpg@users.noreply.github.com>
2019-12-16 21:10:17 +0100

    implementacja niepełna

--- Net/src/storage_types.cpp
+++ Net/src/storage_types.cpp
@@ -9,6 +9,36 @@
 #include "storage_types.hpp"
 
 
+void PackageQueue::push(Package&& package) {
+    switch(storage_type_) {
+        case PackageQueueType::FIFO:
+            pdeq_.emplace_back(std::move(package));
+            break;
+        case PackageQueueType::LIFO:
+            pdeq_.emplace_front(std::move(package));
+            break;
+
+    }
+
+}
+
+Package PackageQueue::pop() {
+    Package product;
+    switch(storage_type_) {
+        case PackageQueueType::FIFO:
+            product=*pdeq_.begin();
+            pdeq_.pop_front();
+            break;
+        case PackageQueueType::LIFO:
+            product=*pdeq_.rend();
+            pdeq_.pop_back();
+            break;
+    }
+    return product;
+}
+
+
+
 #endif //NET_STORAGE_TYPES_HPP
 
 
