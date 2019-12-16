a340f58a0caa559aeaa3a240365f70b10c6f93c6
krzkaszinpg <49557289+krzkaszinpg@users.noreply.github.com>
2019-12-16 18:52:19 +0100

    poprawa

--- Net/src/package.cpp
+++ Net/src/package.cpp
@@ -1,23 +1,53 @@
+// #include "package.hpp"
+
+// std::set<ElementID> Package::assigned_IDs;
+// std::set<ElementID> Package::freed_IDs;
+
+// Package::Package() {
+//     // Take first el. from freed_IDs <set>, if it exists else take last from assigned_IDs. When assigned_IDs is empty first el. = 1.
+//     // Wez pierwszy el. z freed_IDs <set>, jezeli istnieje w przeciwnym wypadku wez ostatni el. z assigned_IDs. Kiedy assigned_IDs jest pusty pierwszym el. = 1
+//     if (!Package::freed_IDs.empty()){
+//         id = *Package::freed_IDs.begin();
+//         Package::freed_IDs.erase(id);
+//     }
+//     else
+//         id = *Package::assigned_IDs.end()+1;
+//     Package::assigned_IDs.insert(id);
+// }
+
+// Package::~Package(){
+//     // Move id form assigned_IDs to freed_IDs
+//     // Przerzuc element z assigned_IDs do freed_IDs
+//     Package::assigned_IDs.erase(id);
+//     Package::freed_IDs.insert(id);
+// }
+
+//
+// Created by krzys on 10.12.2019.
+//
+
 #include "package.hpp"
 
 std::set<ElementID> Package::assigned_IDs;
 std::set<ElementID> Package::freed_IDs;
 
 Package::Package() {
-    // Take first el. from freed_IDs <set>, if it exists else take last from assigned_IDs. When assigned_IDs is empty first el. = 1.
-    // Wez pierwszy el. z freed_IDs <set>, jezeli istnieje w przeciwnym wypadku wez ostatni el. z assigned_IDs. Kiedy assigned_IDs jest pusty pierwszym el. = 1
-    if (!Package::freed_IDs.empty()){
-        id = *Package::freed_IDs.begin();
-        Package::freed_IDs.erase(id);
+    if (!freed_IDs.empty()) {
+        id_= *freed_IDs.begin();
+        freed_IDs.erase(id_);
+        assigned_IDs.insert(id_);
     }
-    else
-        id = *Package::assigned_IDs.end()+1;
-    Package::assigned_IDs.insert(id);
+    else if (!assigned_IDs.empty()) {
+        id_= *assigned_IDs.rbegin()+1;
+        assigned_IDs.insert(id_);
+    }
+    else {
+        id_=0;
+        assigned_IDs.insert(id_);
+    }
 }
 
-Package::~Package(){
-    // Move id form assigned_IDs to freed_IDs
-    // Przerzuc element z assigned_IDs do freed_IDs
-    Package::assigned_IDs.erase(id);
-    Package::freed_IDs.insert(id);
+Package::~Package() {
+    assigned_IDs.erase(id_);
+    freed_IDs.insert(id_);
 }
