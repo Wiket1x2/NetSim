df36640b3d835e83cf8c948c837f81b05a9b0ad0
Rafał <rafaline@vp.pl>
2019-12-16 19:06:28 +0100

    Package

--- Net/include/package.hpp
+++ Net/include/package.hpp
@@ -2,8 +2,8 @@
 // Created by krzys on 10.12.2019.
 //
 
-/*#ifndef NET_SIM_PACKAGE_HPP
-#define NET_SIM_PACKAGE_HPP
+#ifndef NET_PACKAGE_HPP
+#define NET_PACKAGE_HPP
 #include <iostream>
 #include <set>
 #include "types.hpp"
@@ -18,6 +18,8 @@ private:
 public:
     Package();
 
+    Package(const Package&) = default;
+
     Package(Package&&) = default;
 
     ElementID get_id() const {return id;}
@@ -27,32 +29,7 @@ public:
     ~Package();
 };
 
-#endif //NET_SIM_PACKAGE_HPP*/
+#endif //NET_PACKAGE_HPP
 
 
-#ifndef NET_SIM_PACKAGE_HPP
-#define NET_SIM_PACKAGE_HPP
 
-#include "types.hpp"
-#include <set>
-
-class Package {
-
-public:
-    Package();
-    //Package(ElementID id): id_(id){} dodac obsluge gdy id jest zajete
-    Package(const Package&)= default; //ktore maja byc const a ktore nie
-    Package(Package&&) = default;
-    Package& operator=(Package&&)= default;
-    ElementID get_id() const { return id_;}
-    ~Package();
-
-
-private:
-    ElementID id_;
-    static std::set<ElementID> assigned_IDs;
-    static std::set<ElementID> freed_IDs;
-
-};
-
-#endif //NET_SIM_PACKAGE_HPP
