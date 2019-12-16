dddef837ad44b83b493ea0e956aa85bee157ba30
Rafał <rafaline@vp.pl>
2019-12-16 19:29:57 +0100

    all

--- Net/include/storage_types.hpp
+++ Net/include/storage_types.hpp
@@ -1,5 +1,5 @@
 //
-// Created by Aayli on 15.12.2019.
+// Created by krzys on 15.12.2019.
 //
 
 #ifndef NET_STORAGE_TYPES_HPP
--- Net/src/storage_types.cpp
+++ Net/src/storage_types.cpp
@@ -2,13 +2,9 @@
 // Created by Aayli on 15.12.2019.
 //
 
-
-#ifndef NET_STORAGE_TYPES_HPP
-#define NET_STORAGE_TYPES_HPP
-
 #include "storage_types.hpp"
 
 
-#endif //NET_STORAGE_TYPES_HPP
+
 
 
