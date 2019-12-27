086049c6fa8e2ddbb31d01580cf2ac122272c84e
Rafał <rafaline@vp.pl>
2019-12-27 22:43:41 +0100

    kosmetyczne+komentarze

--- Net/include/helpers.hpp
+++ Net/include/helpers.hpp
@@ -13,7 +13,7 @@ std::random_device rd;
 std::mt19937 rng(rd());
 
 double probability_generator_1() {
-    return std::generate_canonical<double, 10>(rng);
+    return std::generate_canonical<double, 10>(rng); //wystepuje problem wielokrotnej deklaracji
 }
 
 double probability_generator_2() {
