dd964c940e3dc59ce2240722a95a155bb15a923f
Rafał <rafaline@vp.pl>
2020-01-09 22:30:20 +0100

    Kueczek_test

--- Net/test/test_storage_types.cpp
+++ Net/test/test_storage_types.cpp
@@ -4,7 +4,12 @@
 
 #include "gtest/gtest.h"
 
+#include "package.hpp"
 #include "storage_types.hpp"
+#include "types.hpp"
+
+using ::std::cout;
+using ::std::endl;
 
 TEST(NetStorage_TypesTest, Lifo) {
     Package A;
@@ -24,4 +29,30 @@ TEST(NetStorage_TypesTest, Fifo) {
     lifo.push((Package&&) B);
     Package D = lifo.pop();
     ASSERT_EQ(D.get_id(),1);
+}
+
+// Kueczek
+
+TEST(PackageQueueTest, IsFifoCorrect) {
+    PackageQueue q(PackageQueueType::FIFO);
+    q.push(Package(1));
+    q.push(Package(2));
+
+    Package p(std::move(q.pop()));
+    EXPECT_EQ(p.get_id(), 1);
+
+    p = q.pop();
+    EXPECT_EQ(p.get_id(), 2);
+}
+
+TEST(PackageQueueTest, IsLifoCorrect) {
+    PackageQueue q(PackageQueueType::LIFO);
+    q.push(Package(1));
+    q.push(Package(2));
+
+    Package p(std::move(q.pop()));
+    EXPECT_EQ(p.get_id(), 2);
+
+    p = q.pop();
+    EXPECT_EQ(p.get_id(), 1);
 }
