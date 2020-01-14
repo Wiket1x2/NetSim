bc8de8ea4ea99e5f84be7b4ad254e0cb3485ce7e
krzkaszinpg <49557289+krzkaszinpg@users.noreply.github.com>
2020-01-14 14:15:03 +0100

    Add files via upload

--- Net/include/factory.hpp
+++ Net/include/factory.hpp
@@ -1,12 +1,11 @@
-//
-// Created by krzys on 10.12.2019.
-//
+// 5: Kasztelewicz (302858), Szczerba (302924), Gorecki (302847)
 
 #ifndef NET_SIM_FACTORY_HPP
 #define NET_SIM_FACTORY_HPP
 
 #include "nodes.hpp"
-
+#include <stdexcept>
+#include <iostream>
 
 
 //TODO: Podzieliłem zadania o podobnej funkcjonalności z moimi sugestiami kto co powinien wziąć.
@@ -14,25 +13,45 @@
 //TODO: Zaznaczcie proszę przy nawiasie z waszym inisiałem "+" - mi pasuje, "-" - wolałbym nie.
 
 //TODO: 4pkt (R)+(K)
+//FIXME UWAGA: Definicje metod klasy szablonowej umieszczamy tutaj
 template <typename Node>
 class NodeCollection{
 public:
+    //FIXME Co z konstruktorem? Domyslny?
     using container_t = typename std::list<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
-    void add(Node& node) { list.push_back(node); };
-//    void remove_by_id(ElementID id);
-    NodeCollection<Node>::iterator find_by_id(ElementID id) { for(iterator it = list.begin(); it != list.end(); ++it) if(it->get_id()==id) return it; return iterator(); } //FIXME: sprawdzić i nullptr(funkcja w sumie nic nie robi) czy throw? (main.cpp)
-    NodeCollection<Node>::const_iterator find_by_id(ElementID id) const { for(const_iterator it = list.begin(); it != list.end(); ++it) if(it->get_id()==id) return it; return iterator(); } //FIXME: sprawdzić i nullptr czy throw?
-    const_iterator cbegin() const { return list.cbegin(); }
-    const_iterator cend() const { return list.cend(); }
-    const_iterator begin() const { return list.cbegin(); }
-    const_iterator end() const { return list.cend(); }
-    iterator begin() { return list.begin(); }
-    iterator end() { return list.end(); }
+
+    void add(Node& node) { list_of_nodes.push_back(std::move(node)); } //TODO problem - kontenery nie moga przechowywac referencji, nie bezposrednio
+
+    NodeCollection<Node>::iterator find_by_id(ElementID id_) {
+        NodeCollection<Node>::iterator it_of_found_node = std::find_if(list_of_nodes.begin(),list_of_nodes.end(),[id_](const Node& elem){ return elem.get_id()==id_;} );
+        return it_of_found_node; //jak nie znajdzie to zwraca it tuz za koncem
+
+//        for(iterator it = list_of_nodes.begin(); it != list_of_nodes.end(); ++it) {
+//            if(it->get_id()==id_) return it; return iterator();}
+    }
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id_) const {
+        NodeCollection<Node>::const_iterator it_of_found_node = std::find_if(list_of_nodes.cbegin(),list_of_nodes.cend(),[id_](const Node& elem){ return elem.get_id()==id_;} );
+        return it_of_found_node;
+
+//        for(const_iterator it = list_of_nodes.begin(); it != list_of_nodes.end(); ++it) if(it->get_id()==id) return it; return iterator();
+    }
+
+    void remove_by_id(ElementID id_) {
+        auto node_to_remove=find_by_id(id_);
+        if (node_to_remove!=list_of_nodes.end()) {list_of_nodes.erase(node_to_remove);}
+    }
+
+    const_iterator cbegin() const { return list_of_nodes.cbegin(); }
+    const_iterator cend() const { return list_of_nodes.cend(); }
+    const_iterator begin() const { return list_of_nodes.cbegin(); }
+    const_iterator end() const { return list_of_nodes.cend(); }
+    iterator begin() { return list_of_nodes.begin(); }
+    iterator end() { return list_of_nodes.end(); }
 
 private:
-    container_t list;
+    container_t list_of_nodes; //FIXME zmieniono nazwe - nie mozna uzywac nazw zarezerowanych dla kontenerow np. list vector itp
 };
 
 
@@ -71,3 +90,4 @@ private:
 //TODO: testy 3pkt (R)
 
 #endif //NET_SIM_FACTORY_HPP
+// 5: Kasztelewicz (302858), Szczerba (302924), Gorecki (302847)
